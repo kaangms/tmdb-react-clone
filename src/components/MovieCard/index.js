a50e6556f73f1682d4de3ca4625aa111aa0fb39f
Kaan Gümüş <hkgms05@gmail.com>
2021-11-05 21:35:36 +0300

    lazy loading add

--- src/components/MovieCard/index.js
+++ src/components/MovieCard/index.js
@@ -1,16 +1,19 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Suspense } from "react";
 import TabBar from "../toolbox/TabBar";
 import "./style.scss";
-// import Background from "../../images/movie-card/moviecard.jpg";
-import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faThumbsUp, faThumbsDown } from "@fortawesome/free-solid-svg-icons";
-import "react-circular-progressbar/dist/styles.css";
 import MovieService from "../../services/MovieService";
-import { IMAGE_BASE_URL, IMAGE_SIZE } from "../../config";
+import LoadImageCard from "../toolbox/ImageCard/ImageCard";
+import RaitingBar from "../toolbox/RaitingBar";
+const ImageCard = React.lazy(() => import("../toolbox/ImageCard/ImageCard"));
+// // const ImageCard = React.lazy(async () => {
+// //   await new Promise((resolve) => setTimeout(resolve, 5 * 1000));
 
-const MovieCard = ({ filmSections }) => {
+// //   return await (Math.floor(Math.random() * 10) >= 4
+// //     ? import("../toolbox/ImageCard/ImageCard")
+// //     : Promise.reject(new Error()));
+// // });
+const MovieCard = () => {
   //TODO:Çok resim geldiği zaman kaydırma eklenecek
 
   const [movies, setMovies] = useState([]);
@@ -34,45 +37,18 @@ const MovieCard = ({ filmSections }) => {
               <div className="column_content flex scroller loaded">
                 {movies.map((movie) => (
                   <div key={movie.id} className="movie-card">
-                    <img
-                      className="movie-image"
-                      src={IMAGE_BASE_URL + IMAGE_SIZE + movie.poster_path}
-                      alt="img"
-                    />
-                    {/* <div
-                  style={{ backgroundImage: `url(${Background})` }}
-                  className="movie-image"
-                ></div> */}
-                    <h4 className="movie-title">{movie.name}</h4>
-                    <h6>{movie.first_air_date}</h6>
-                    <div className="movie-rating">
-                      <CircularProgressbar
-                        value={raiting(movie.vote_average)}
-                        text={`${raiting(movie.vote_average)}%`}
-                        background
-                        backgroundPadding={6}
-                        styles={buildStyles({
-                          backgroundColor: "black",
-                          textColor: "white",
-                          pathColor: RatingColor(
-                            parseInt(raiting(movie.vote_average))
-                          ),
-                          trailColor: "transparent",
-                        })}
+                    <Suspense fallback={<LoadImageCard isLoading={true} />}>
+                      <ImageCard
+                        path={movie.poster_path}
+                        className="movie-image"
                       />
-                    </div>
-                    <div className="fa-thumbs">
-                      <FontAwesomeIcon
-                        id="fa-thumbs-up"
-                        icon={faThumbsUp}
-                        color="gray"
-                      />{" "}
-                      <FontAwesomeIcon
-                        id="fa-thumbs-down"
-                        icon={faThumbsDown}
-                        color="gray"
+                      <h4 className="movie-title">{movie.name}</h4>
+                      <h6>{movie.first_air_date}</h6>
+                      <RaitingBar
+                        className="movie-rating"
+                        vote_average={movie.vote_average}
                       />
-                    </div>
+                    </Suspense>
                   </div>
                 ))}
               </div>
@@ -85,16 +61,6 @@ const MovieCard = ({ filmSections }) => {
 };
 export default MovieCard;
 
-function RatingColor(param) {
-  if (param >= 75 && param <= 100) return "green";
-  else if (param >= 50 && param < 75) return "yellow";
-  else if (param >= 0 && param < 50) return "yellow";
-  else return "black";
-}
-function raiting(rate) {
-  //10 üzerinden rate
-  return rate * 10;
-}
 const tabNames = [
   {
     id: 1,
